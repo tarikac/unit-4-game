6ee17c678342e4ab1da072558200c6d60c672ddc
tarikac <tarika.common@gmail.com>
2019-04-08 14:33:57 -0500

    buttons

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+//set my variables
+//i neeed randomNumber picker
+//need to set random numbers for each button
+//need a total that tracks the numbers combined for random numbers
+//need counter for wins
+//need counter for loses
+
+
+//on click for each button. Should I send this to a function?
+
+
+//total of points being made
+
+
+//track winnings and display them (if matches randomNumber) (this should also reset crystals and randNumber)
+
+//track losses and display them (if number is higher than randomNumber..also resets crystals)
+
+//reset game gives a new randomNumber and a new number for each crystal
+
+
+
+
+});
+
