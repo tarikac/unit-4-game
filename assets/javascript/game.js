bf4af9da1123831025f0376be4056d2102376f38
tarikac <tarika.common@gmail.com>
2019-04-10 15:55:06 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,8 +16,8 @@ console.log(randomCrystalTwo);
 console.log(randomCrystalThree);
 console.log(randomCrystalFour);
 
-$("#randomcapture").text(randomNumber).css("font-size", "48px");
-$("#currenttotal").text(total).css("font-size", "48px");
+$("#randomcapture > p").text(randomNumber);
+$("#currenttotal > p").text(total);
 $("#wins").text("Number of Wins: " + wins);
 $("#losses").text("Number of Losses: " + losses);
 
